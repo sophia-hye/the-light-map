05684257005e6792128809fe145d8b6f3841df50
sophia <kimjihae28@gmail.com>
2025-04-11 17:37:32 +0900

    feat: button and button's font size

--- src/components/atoms/OpenFormButton.tsx
+++ src/components/atoms/OpenFormButton.tsx
@@ -8,17 +8,22 @@ interface OpenFormButtonProps {
 export default function OpenFormButton({ setIsOpen }: OpenFormButtonProps) {
   // when clicked, show form to add my star on modal window
   return (
-    <Style.Button onClick={() => setIsOpen(true)}>+ My Star</Style.Button>
+    <Style.Container>
+      <Style.Button onClick={() => setIsOpen(true)}>+ My Star</Style.Button>
+    </Style.Container>
   )
 }
 
 const Style = {
+  Container: styled.div`
+    /* font-size: 8px; */
+  `,
   Button: styled.button`
     position: fixed;
     top: 20px;
     right: 20px;
-    width: 50px;
-    height: 50px;
+    width: 30px;
+    height: 30px;
     background-color: #000;
     color: #fff;
     border-radius: 50%;
@@ -28,7 +33,7 @@ const Style = {
                 0 0 20px rgba(255, 255, 255, 0.3),
                 0 0 30px rgba(255, 255, 255, 0.1);
     transition: transform 0.2s, box-shadow 0.3s;
-    font-size: 8px;
+    font-size: 0.35rem;
 
     &:hover {
       transform: scale(1.05);
